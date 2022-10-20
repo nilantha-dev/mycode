20cdc9d16a12425a610a51844fe0eabcec194489
nilantha <nilantha-dev@users.noreply.github.com>
2022-10-20 21:52:21 +0000

    Outputting files

---
+++ 99bottles.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   For - Using a file's lines as a source for the for-loop"""
+
+for bottles in range(99,0,-1):
+    print(bottles, " bottles of beer on the wall!\n",
+            bottles, " bottles of beer on the wall!", 
+            bottles," bottles of beer! You take one down, pass it around!")
